ec9b80ada46f090a857fc4cd9a7157bd84690802
RomThpt <reeflk8@gmail.com>
2025-12-19 18:40:03 +0100

    Add Vault contract for ETH deposits and rebase token management

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -3,31 +3,37 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-/*
-* @title RebaseToken
-* @author RomThpt
-* @notice A cross-chain rebase token that incentivises users to depostit into a vault and gain interest in rewards
-* @notice The interest rate in the smart contract can only decrease
-* @notice Each user will have their own interest rate taht is the global interest rate at the time of their deposit
-*/
-contract RebaseToken is ERC20, Ownable {
+/**
+ * @title RebaseToken
+ * @author RomThpt
+ * @notice A cross-chain rebase token that incentivises users to depostit into a vault and gain interest in rewards
+ * @notice The interest rate in the smart contract can only decrease
+ * @notice Each user will have their own interest rate taht is the global interest rate at the time of their deposit
+ */
+contract RebaseToken is ERC20, Ownable, AccessControl {
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 newInterestRate, uint256 currentInterestRate);
 
     uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
     uint256 private s_interestRate = 5e10;
     mapping(address => uint256) private s_userInterestRates;
     mapping(address => uint256) private s_userLastUpdatedTimestamps;
 
     event InterestRateUpdated(uint256 newInterestRate);
 
-    constructor(uint256 _initialInterestRate) ERC20("RebaseToken", "RBT") {}
+    constructor(uint256 _initialInterestRate) ERC20("RebaseToken", "RBT") Ownable(msg.sender) {}
 
-    /*
-    * @notice Set the interest rate in the contract
-    * @param _newInterestRate The new interest rate to be set
-    * @dev The new interest rate can only be lower than the current interest rate
-    */
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
+    }
+
+    /**
+     * @notice Set the interest rate in the contract
+     * @param _newInterestRate The new interest rate to be set
+     * @dev The new interest rate can only be lower than the current interest rate
+     */
     function setInterestRate(uint256 _newInterestRate) external onlyOwner {
         // Implementation for setting a new interest rate
         if (_newInterestRate >= s_interestRate) {
@@ -47,25 +53,37 @@ contract RebaseToken is ERC20, Ownable {
         return super.balanceOf(_user);
     }
 
-
-    /*
-    * @notice Mint rebase tokens to a user
-    * @dev Internal function to mint tokens
-    * @param _to The address to mint tokens to
-    * @param _amount The amount of tokens to mint
-    */
-    function mint(address _to, uint256 _amount) external {
+    /**
+     * @notice Mint rebase tokens to a user
+     * @dev Internal function to mint tokens
+     * @param _to The address to mint tokens to
+     * @param _amount The amount of tokens to mint
+     */
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
         _mintAccruedInterest(_to);
         s_userInterestRates[_to] = s_interestRate;
         _mint(_to, _amount);
     }
 
     /*
-    * @notice Get the balance of a user including accrued interest
-    * @dev Override the balanceOf function to include accrued interest
-    * @param _user The address of the user
-    * @return The balance of the user including accrued interest
+    * @notice Burn rebase tokens from a user
+    * @param _from The address to burn tokens from
+    * @param _amount The amount of tokens to burn
     */
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_from);
+        }
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
+    }
+
+    /**
+     * @notice Get the balance of a user including accrued interest
+     * @dev Override the balanceOf function to include accrued interest
+     * @param _user The address of the user
+     * @return The balance of the user including accrued interest
+     */
     function balanceOf(address _user) public view override returns (uint256) {
         //get the current pricipal balance (the number that have actually been minted to the user)
         // multiply the principal by (1 + interest rate * time elapsed / seconds in a year)
@@ -93,36 +111,33 @@ contract RebaseToken is ERC20, Ownable {
         return super.transfer(_to, _amount);
     }
 
-    /*
-    * @notice Transfer rebase tokens from one user to another on behalf of a third party
-    * @dev Override the transferFrom function to mint accrued interest before transferring
-    * @param _from The address to transfer tokens from
-    * @param _to The address to transfer tokens to
-    * @param _amount The amount of tokens to transfer
-    * @return A boolean value indicating whether the operation succeeded
-    * @notice This function mints accrued interest to both the sender and recipient before transferring tokens
-    */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _amount
-    ) public override returns (bool) {
+    /**
+     * @notice Transfer rebase tokens from one user to another on behalf of a third party
+     * @dev Override the transferFrom function to mint accrued interest before transferring
+     * @param _from The address to transfer tokens from
+     * @param _to The address to transfer tokens to
+     * @param _amount The amount of tokens to transfer
+     * @return A boolean value indicating whether the operation succeeded
+     * @notice This function mints accrued interest to both the sender and recipient before transferring tokens
+     */
+    function transferFrom(address _from, address _to, uint256 _amount) public override returns (bool) {
         _mintAccruedInterest(_from);
         _mintAccruedInterest(_to);
         if (_amount == type(uint256).max) {
             _amount = balanceOf(_from);
         }
-        if( balanceOf(_to) == 0) {
+        if (balanceOf(_to) == 0) {
             s_userInterestRates[_to] = s_userInterestRates[_from];
         }
         return super.transferFrom(_from, _to, _amount);
+    }
 
-    /*
-    * @notice Calculate the accumulated interest for a user since their last update
-    * @dev Internal function to calculate accumulated interest
-    * @param _user The address of the user
-    * @return The accumulated interest multiplier
-    */
+    /**
+     * @notice Calculate the accumulated interest for a user since their last update
+     * @dev Internal function to calculate accumulated interest
+     * @param _user The address of the user
+     * @return The accumulated interest multiplier
+     */
     function _calculateUserAccumulatedInterestSinceLastUpdate(address _user)
         internal
         view
@@ -139,11 +154,11 @@ contract RebaseToken is ERC20, Ownable {
         return linearInterest;
     }
 
-    /*
-    * @notice Mint accrued interest to a user since the last time they performed an action
-    * @dev Internal function to mint accrued interest based on the user's interest rate
-    * @param _user The address of the user to mint interest to
-    */
+    /**
+     * @notice Mint accrued interest to a user since the last time they performed an action
+     * @dev Internal function to mint accrued interest based on the user's interest rate
+     * @param _user The address of the user to mint interest to
+     */
     function _mintAccruedInterest(address _user) internal {
         // (1) find the current balance of rebase token that have been minted to them -> pricipal
         uint256 previousPrincipal = super.balanceOf(_user);
@@ -159,33 +174,20 @@ contract RebaseToken is ERC20, Ownable {
         }
     }
 
-    /*
-    * @notice Burn rebase tokens from a user
-    * @param _from The address to burn tokens from
-    * @param _amount The amount of tokens to burn
-    */
-    function burn(address _from, uint256 _amount) external {
-        if (_amount == type(uint256).max) {
-            _amount = balanceOf(_from);
-        }
-        _mintAccruedInterest(_from);
-        _burn(_from, _amount);
-    }
-
     /*****GETTERS******/
 
-    /*
-    * @notice Get the current global interest rate
-    * @return The current global interest rate
-    */
+    /**
+     * @notice Get the current global interest rate
+     * @return The current global interest rate
+     */
     function getUserInterestRate(address _user) external view returns (uint256) {
         return s_userInterestRates[_user];
     }
 
-    /*
-    * @notice Get the current global interest rate
-    * @return The current global interest rate
-    */
+    /**
+     * @notice Get the current global interest rate
+     * @return The current global interest rate
+     */
     function getInterestRate() external view returns (uint256) {
         return s_interestRate;
     }
---
+++ src/Vault.sol
@@ -0,0 +1,54 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
+
+contract Vault {
+    IRebaseToken private immutable i_rebaseToken;
+
+    event Deposit(address indexed user, uint256 amount);
+
+    error Vault__RedeemFailed();
+
+    constructor(IRebaseToken _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+    /**
+     * @notice Fallback function to accept ETH deposits
+     * @dev This function allows the contract to accept ETH deposits directly
+     */
+    receive() external payable {}
+
+    /**
+     * @notice Deposit ETH into the vault and receive rebase tokens
+     * @dev This function mints rebase tokens to the sender based on the amount of ETH deposited
+     * @param _amount The amount of ETH to deposit
+     * emits a Deposit event
+     */
+    function deposit() external payable {
+        i_rebaseToken.mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    /**
+     * @notice Redeem rebase tokens for ETH from the vault
+     * @dev This function burns rebase tokens from the sender and sends ETH back to them
+     * @param _amount The amount of rebase tokens to redeem
+     * emits a Redeem event
+     */
+    function redeem(uint256 _amount) external {
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+    }
+
+    /**
+     * @notice Get the address of the rebase token contract
+     * @return The address of the rebase token contract
+     */
+    function getRebaseTokenAddress() external view returns (address) {
+        return address(i_rebaseToken);
+    }
+}
